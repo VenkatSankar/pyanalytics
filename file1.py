26782be32b10b5b944b3931f422a85f889ac8d75
venkatsankar <svx2021@email.iimcal.ac.in>
2020-07-20 19:41:34 +0530

    Changed

---
+++ file1.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+line1 = [1,2,3]
+print(line1)
+
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+line1 = [1,2,3]
+print(line1)
